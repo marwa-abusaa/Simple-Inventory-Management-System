c5d0799d7f1a38b2e42550fa4e0d448702aa22be
marwa-abusaa <s12028718@stu.najah.edu>
2025-02-23 17:46:55 +0200

    Implement addProduct method

--- Inventory.cs
+++ Inventory.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 namespace Simple_Inventory_Management_System
 {
     class Inventory
-    { 
+    {
+        private List<Product> products = [];
+        public void addProduct(string name, double price, int quantity)
+        {
+            products.Add(new Product(name, price, quantity));
+        }
+
     }
 }

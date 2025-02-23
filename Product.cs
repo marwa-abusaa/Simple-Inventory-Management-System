b76e35d2fe7008ea429944f7a6c77a1e775a101e
marwa-abusaa <s12028718@stu.najah.edu>
2025-02-23 17:43:53 +0200

    add product class properties

--- Product.cs
+++ Product.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace Simple_Inventory_Management_System
 {
     class Product
-    { 
+    {
+        private string name;
+        private double price;
+        private int quantity;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        public Product(string name, double price, int quantity)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
     }
 }

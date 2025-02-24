979f23457d28e69391c0cbd10246699068d409a2
marwa-abusaa <s12028718@stu.najah.edu>
2025-02-24 15:28:06 +0200

    implement menu-edit product

--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@ class Program
 {
     static void AddNewProduct(Inventory inventory)
     {
-        Console.Write("Enter product name: ");
-        string name = Console.ReadLine();
+        string name ="";
+        
+        while (true)
+        {
+            Console.Write("Enter product name: ");
+            name = Console.ReadLine();
+            if (name!=string.Empty)
+                break;
+            Console.WriteLine("Please enter the name of product.");
+        }
 
         double price;
         while (true)
@@ -51,6 +59,121 @@ class Program
         //search product
         inventory.SearchProduct(name);
     }
+    //static void EditProduct(Inventory inventory)
+    //{
+    //    Console.Write("Enter product name: ");
+    //    string name = Console.ReadLine();
+
+    //    Console.Write("What do you want to update? ");
+    //    EditMenu();
+    //    string choice = Console.ReadLine();
+    //    string field="";
+    //    switch (choice)
+    //    {
+    //        case "1":
+    //            {
+    //                field = "Name";
+    //                Console.Write("Enter new name: ");
+    //                break;
+    //            }
+                
+    //        case "2":
+    //            {
+    //                field = "Price";
+    //                Console.Write("Enter new price: ");
+    //                break;
+    //            }
+    //        case "3":
+    //            {
+    //                field = "Quantity";
+    //                Console.Write("Enter new quantity: ");
+    //                break;
+    //            }
+    //    }
+
+    //    string newValue = Console.ReadLine();
+    //    //edit product
+    //    inventory.EditProduct(name, field, newValue);
+
+    //}
+
+    static void EditProduct2(Inventory inventory)
+    {
+        Console.Write("Enter product name: ");
+        string name = Console.ReadLine();
+
+        if (inventory.Search(name)==null)
+        {
+            return;
+        }
+
+        Console.Write("What do you want to update? ");
+        EditMenu();
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                {
+                    while (true)
+                    {
+                        Console.Write("Enter new name: ");
+                        string newName = Console.ReadLine();
+                        if (newName != string.Empty)
+                        {
+                            inventory.EditName(newName);
+                            break;
+                        }
+                        Console.WriteLine("Please enter the name of product:");
+                    }
+                    break;
+                }
+
+            case "2":
+                {
+                    double newPrice;
+                    while (true)
+                    {
+                        Console.Write("Enter new price: ");
+                        if (double.TryParse(Console.ReadLine(), out newPrice) && newPrice >= 0)
+                        {
+                            inventory.EditPrice(name, newPrice);
+                            break;
+                        }
+                        Console.WriteLine("Invalid input! Please enter a number:");
+                    }
+                    break;
+                }
+            case "3":
+                {
+                    int newQuantity;
+                    while (true)
+                    {
+                        Console.Write("Enter new quantity: ");
+                        if (int.TryParse(Console.ReadLine(), out newQuantity) && newQuantity >= 0)
+                        {
+                            inventory.EditQuantity(name, newQuantity);
+                            break;
+                        }
+                        Console.WriteLine("Invalid input! Please enter a number:");
+                    }
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("Please, choose a valid number!");
+                    break;
+                }               
+               
+        }
+
+    }
+    static void EditMenu()
+    {
+        Console.WriteLine("\n1. Product Name");
+        Console.WriteLine("2. Product Price");
+        Console.WriteLine("3. Product Quantity");
+        Console.Write("Choose an option: ");
+    }
     static void Menu()
     {
         Console.WriteLine("\n1. Add Product");
@@ -75,12 +198,12 @@ class Program
             {
                 case "1":
                     AddNewProduct(inventory);
-                        break;
-                                      
+                        break;                                     
                 case "2":
                     ViewProducts(inventory);
                     break;
                 case "3":
+                    EditProduct2(inventory);
                     break;
                 case "4":
                     DeleteProduct(inventory);
@@ -92,8 +215,7 @@ class Program
                     return;
                 default:
                     {
-                        Console.WriteLine("Please, choose a correct number!");
-                        choice = Console.ReadLine();
+                        Console.WriteLine("Please, choose a valid number!");
                         break;
                     }
 

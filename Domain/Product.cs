80360844f4500033da2ea5cd70d36b4b2b2c4ac4
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-05 10:06:37 +0200

    Refactor Product class

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
+﻿
 namespace Simple_Inventory_Management_System.Domain
 {
-    class Product
-    {
-        private string name;
-        private double price;
-        private int quantity;
+    public class Product
+    {      
         public string Name { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
+
         public Product(string name, double price, int quantity)
         {
             Name = name;

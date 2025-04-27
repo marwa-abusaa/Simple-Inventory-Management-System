c3d9a960127454731c0c0c66bffe716e208cd3b8
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-27 16:12:47 +0300

    Create DataBaseConnection class to manage SQL Server connection string

---
+++ Infrastructure/DataBaseConnection.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Simple_Inventory_Management_System.Infrastructure;
+
+public class DataBaseConnection
+{
+    public static string connectionString = "Server=LAPTOP-G8RBUTE9;Database=Inventory_Management_System;Trusted_Connection=True;";
+}

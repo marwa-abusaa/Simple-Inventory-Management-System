2efcd918c61ff379aa0a77c577018abf0464403e
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-27 18:37:33 +0300

    remove unnecessary files

--- Domain/Inventory.cs
+++
@@ -1,8 +0,0 @@
-﻿using Simple_Inventory_Management_System.Infrastructure;
-
-namespace Simple_Inventory_Management_System.Domain;
-
-public class Inventory
-{
-    private string connectionString = DataBaseConnection.connectionString;
-}

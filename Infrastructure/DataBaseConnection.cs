55535866d19e995fa93987559a365e378145a8ea
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-27 18:24:51 +0300

    update connectionString to include TrustServerCertificate

--- Infrastructure/DataBaseConnection.cs
+++ Infrastructure/DataBaseConnection.cs
@@ -3,5 +3,5 @@ namespace Simple_Inventory_Management_System.Infrastructure;
 
 public class DataBaseConnection
 {
-    public static string connectionString = "Server=LAPTOP-G8RBUTE9;Database=Inventory_Management_System;Trusted_Connection=True;";
+    public static string connectionString = "Server=LAPTOP-G8RBUTE9;Database=Inventory_Management_System;Trusted_Connection=True;TrustServerCertificate=True;";
 }

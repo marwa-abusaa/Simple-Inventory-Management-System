cf04a1f8f8d60aab778f62e0696ca73cbea4de7a
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-01 15:29:35 +0300

    Refactor: Move DB logic from service to repository class in infrastructure for better separation of concerns

---
+++ Infrastructure/ProductRepository.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Data.SqlClient;
+using Simple_Inventory_Management_System.Domain;
+using Simple_Inventory_Management_System.Services;
+
+
+namespace Simple_Inventory_Management_System.Infrastructure;
+
+public class ProductRepository
+{
+    private string connectionString = DataBaseConnection.ConnectionString;
+
+    public void AddProduct(string name, double price, int quantity)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "INSERT INTO Products (Name, Price, Quantity) VALUES (@Name, @Price, @Quantity)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Price", price);
+                command.Parameters.AddWithValue("@Quantity", quantity);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public Product? Search(string name)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT * FROM Products WHERE Name = @Name";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Name", name);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Product product = new Product
+                        (
+                            reader["Name"].ToString()!,
+                            Convert.ToDouble(reader["Price"]),
+                            Convert.ToInt32(reader["Quantity"])
+                        );
+
+                        return product;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public void SearchProduct(string name)
+    {
+        Product? product = Search(name);
+        if (product != null)
+        {
+            ProductService.DisplayProductDetails(product);
+        }
+    }
+
+    public List<Product> DisplayAllProducts()
+    {
+        List<Product> products = new List<Product>();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT * FROM Products";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Product product = new Product
+                        (
+                            reader["Name"].ToString()!,
+                            Convert.ToDouble(reader["Price"]),
+                            Convert.ToInt32(reader["Quantity"])
+                        );
+
+                        products.Add(product);
+                    }
+                }
+            }
+        }
+        return products;
+    }
+
+    public bool DeleteProduct(string name)
+    {
+        Product? product = Search(name);
+        if (product != null)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "DELETE FROM Products WHERE Name = @Name";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.ExecuteNonQuery();
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public void EditProduct(string name, double newPrice, int newQuantity, string newName)
+    {
+        Product? product = Search(name);
+        if (product != null)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "UPDATE Products SET Name = @NewName, Price = @NewPrice, Quantity = @NewQuantity WHERE Name = @OldName";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@NewName", newName);
+                    command.Parameters.AddWithValue("@NewPrice", newPrice);
+                    command.Parameters.AddWithValue("@NewQuantity", newQuantity);
+                    command.Parameters.AddWithValue("@OldName", name);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
